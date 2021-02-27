48a276f3899c0a0b35182c0896abef3a072bf5b2
crispyyv <lamer079@gmail.com>
2021-02-27 22:30:13 +0200

    remove cors link

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
-export const CDN_URL =
-  "https://cors-anywhere.herokuapp.com/https://api.hnpwa.com/v0/";
+export const CDN_URL = "https://api.hnpwa.com/v0/";
 
 export const field = ["title", "domain", "date"];
 export const links = ["news", "newest", "ask", "jobs", "show"];
